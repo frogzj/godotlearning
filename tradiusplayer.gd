f70d0bd07259c623c3fb78ea34bcd590e98a0bc2
frogzj <femboykkj@gmail.com>
2025-01-01 16:09:38 -0300

    Delete tradiusplayer.gd

--- tradiusplayer.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody3D
-
-@onready var TRadiusLayer1Area = $TRadius
-@onready var tradiuslayer1 = $"TRadius/CollisionShape3D/TRadius player/Layer1"
-@onready var tradiuslayer2 = $"TRadius/CollisionShape3D/TRadius player/Layer2"
-##@onready var player = 
