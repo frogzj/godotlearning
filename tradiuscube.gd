63a706ef60c7ab5e8d4578a2c93840bab1b54b07
frogzj <femboykkj@gmail.com>
2025-01-01 16:09:03 -0300

    it works now

--- tradiuscube.gd
+++ tradiuscube.gd
@@ -1,14 +1,61 @@
 extends Node3D
 
+var tradiusplay = false
+@onready var tradiuslayer2 = $Layer2/tradius_layer2
 @onready var tradiuslayer1 = $Area3D/tradius_layer1
-var volumeplay = 0
-var volumezero = -80
+@onready var tradiuslayer3 = $Layer3/tradius_layer3
+func _ready() -> void:
+	await 2
+	tradiuslayer1.stop()
+	tradiuslayer2.stop()
+	tradiuslayer3
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	tradiuslayer1.volume_db = 0
 
-func _on_area_3d_body_exited(body: Node3D) -> void:
-	tradiuslayer1.volume_db = -80
+func _on_layer_1_body_entered(body: Node3D) -> void:
+	if tradiusplay == false:
+		tradiusplay = true
+		tradiuslayer1.play()
+		tradiuslayer2.play()
+		tradiuslayer3.play()
+	var tween = get_tree().create_tween()
+	tween.tween_property(tradiuslayer1, "volume_db", 0, 2)
+	tween.play()
+	await tween.finished
+
+func _on_layer_1_body_exited(body: Node3D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(tradiuslayer1, "volume_db", -120, 2)
+	tween.play()
+	await tween.finished
+	
+
+func _on_layer_2_body_entered(body: Node3D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(tradiuslayer2, "volume_db", 0, 0.8)
+	tween.tween_property(tradiuslayer1, "volume_db", -120, 2)
+	tween.play()
+	await tween.finished
+
+func _on_layer_2_body_exited(body: Node3D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(tradiuslayer1, "volume_db", 0, 2)
+	tween.tween_property(tradiuslayer2, "volume_db", -120, 2)
+	tween.play()
+	await tween.finished
+
+func _on_layer_3_body_entered(body: Node3D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(tradiuslayer3, "volume_db", 0, 0.8)
+	tween.tween_property(tradiuslayer2, "volume_db", -120, 2)
+	tween.play()
+	await tween.finished
+
+func _on_layer_3_body_exited(body: Node3D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(tradiuslayer2, "volume_db", 0, 2)
+	tween.tween_property(tradiuslayer3, "volume_db", -120, 2)
+	tween.play()
+	await tween.finished
